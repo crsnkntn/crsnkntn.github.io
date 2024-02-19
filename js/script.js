492300e30178f4a27f8a58d5a106dbbbc41f2313
Carson Ellis <crsnkntn@gmail.com>
2024-02-19 00:30:43 -0500

    reduce, reuse, recycle

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("copyLink").addEventListener("click", function(event) {
-    event.preventDefault();
-    navigator.clipboard.writeText("crsnkntn@gmail.com").then(function() {
-        alert("My email was copied to your clipboard!");
-    }).catch(function(error) {
-        alert("Error copying text: " + error);
-    });
-});
